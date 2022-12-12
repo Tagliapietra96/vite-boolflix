a83d30dcbfa8c85fd023408784a86028fedcb58b
Tagliapietra96 <tagliatek96@gmail.com>
2022-12-12 21:02:09 +0100

    fixed same original and commercial name

--- src/components/SingleElement.vue
+++ src/components/SingleElement.vue
@@ -1,14 +1,14 @@
 <template>
     <li class="list-unstyled text-center" v-if="isMovie"> 
-        <h2>{{ element.original_title }}</h2>
-        <h2>{{ element.title }}</h2>
+        <h2 v-if="element.original_title !== element.title">{{ element.original_title }}</h2>
+        <h2 >{{ element.title }}</h2>
         <h3>{{ element.vote_average }}</h3>
         <p><span :class="'fi fi-' + getFlag(element.original_language)"></span></p>
         <img v-if="element.backdrop_path" :src="store.dt.imgBase + element.backdrop_path">
         <h1 v-else class="text-danger">Img not avaible</h1>
     </li>
     <li class="list-unstyled text-center" v-else> 
-        <h2>{{ element.original_name }}</h2>
+        <h2 v-if="element.original_name !== element.name">{{ element.original_name }}</h2>
         <h2>{{ element.name }}</h2>
         <h3>{{ element.vote_average }}</h3>
         <p><span :class="'fi fi-' + getFlag(element.original_language)"></span></p>
@@ -39,6 +39,8 @@ export default {
                 return 'ua';
             }else if (country === 'hi'){
                 return 'in';
+            }else if (country === 'ja'){
+                return 'jp';
             }else{
                 return country;
             }
